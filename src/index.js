0329dc7b8d90465d3930ce575c525cf114b9b3ee
101686 <73076483+101686@users.noreply.github.com>
2021-11-15 11:26:32 +0100

    VR

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
 import { Mesh } from "@babylonjs/core/scene";
-import { SixDofDragBehavior } from "@babylonjs/core";
+import { DeviceOrientationCamera, SixDofDragBehavior } from "@babylonjs/core";
 import { Vector3 } from "@babylonjs/core/Maths/math";
 import { DirectionalLight } from "@babylonjs/core/Lights/directionalLight";
 import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
@@ -11,29 +11,68 @@ import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
 import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
 import { GLTFLoader } from "@babylonjs/loaders/glTF/2.0/glTFLoader";
 import { Animation } from "@babylonjs/core";
-// Basic setup
-const canvas = document.querySelector("#app");
-const engine = new Engine(canvas, true, null, true);
-const scene = new Scene(engine);
 
-const camera = new ArcRotateCamera(
-  "ArcRotateCamera",
-  2,
-  1.45,
-  4,
-  new Vector3(0, 0, 0),
-  scene
-);
-camera.panningSensibility = 0;
-scene.activeCamera.attachControl(canvas, false);
+var initSetup = async function () {
+  // Basic setup
+  const canvas = document.querySelector("#app");
+  const engine = new Engine(canvas, true, null, true);
+  const scene = new Scene(engine);
 
-new DirectionalLight("dir01", new Vector3(0.25, -1, 0), scene);
+  //vytoření kamery v pozici -5 (dozadu)
+  //const camera = new UniversalCamera("Camera", new Vector3(0, 5, 10), scene);
+  //const camera = new UniversalCamera("kamera",new Vector3(1,1,10),scene);
+  const camera = new DeviceOrientationCamera(
+    "kamera",
+    new Vector3(1, 1, 10),
+    scene
+  );
 
-window.addEventListener("resize", () => engine.resize());
+  //zaměřit kameru do středu
+  camera.setTarget(new Vector3(0, 1, 0));
+  scene.activeCamera.attachControl(canvas, false);
+  const environment1 = scene.createDefaultEnvironment({
+    enableGroundShadow: true
+  });
+  await scene.createDefaultXRExperienceAsync({
+    floorMeshes: [environment1.ground]
+  });
+  new DirectionalLight("dir01", new Vector3(0.25, -1, 0), scene);
 
-engine.runRenderLoop(() => scene.render());
+  window.addEventListener("resize", () => engine.resize());
 
-var initFunction = async function () {
+  engine.runRenderLoop(() => scene.render());
+  return scene;
+};
+
+var initModel2Function = async function (scene) {
+  var freza1 = await SceneLoader.ImportMeshAsync(
+    "",
+    "public/",
+    "mill.glb",
+    scene
+  );
+  // .then(function(newMeshes){
+  //   newMeshes.meshes[0].scaling = new Vector3(0.1, 0.1, 0.07);
+  //   newMeshes.meshes[0].rotate(new Vector3(-1, 0, 0), Math.PI / 2);
+  //   //newMeshes.meshes[0].position.x = 2;
+  //   newMeshes.meshes[0].position.z = 2;
+  // });
+  var frezaMesh1 = freza1.meshes[0];
+  frezaMesh1.rotate(new Vector3(0, 0, 1), (frezaMesh1.rotation.y += 0.00001));
+
+  frezaMesh1.scaling = new Vector3(0.1, 0.1, 0.07);
+  frezaMesh1.rotate(new Vector3(-1, 0, 0), Math.PI / 2);
+  frezaMesh1.position.x = -1;
+  frezaMesh1.position.z = -2;
+  var sixDofDragBehavior = new SixDofDragBehavior();
+  sixDofDragBehavior.rotateDraggedObject = true;
+  // sixDofDragBehavior. = false;
+  let frezasubmesh;
+  frezasubmesh = frezaMesh1;
+  frezasubmesh.addBehavior(sixDofDragBehavior);
+};
+
+var initModelFunction = async function (scene) {
   var frezaMeshaset = await SceneLoader.ImportMeshAsync(
     "",
     "public/",
@@ -47,54 +86,52 @@ var initFunction = async function () {
   frezaMesh2.rotate(new Vector3(-1, 0, 0), Math.PI / 2);
   frezaMesh2.position.x = 2;
   frezaMesh2.position.z = -2;
-  var sixDofDragBehavior = new SixDofDragBehavior();
-  sixDofDragBehavior.rotateDraggedObject = true;
-  // sixDofDragBehavior. = false;
-  let frezasubmesh;
-  frezasubmesh = frezaMesh2;
-  frezasubmesh.addBehavior(sixDofDragBehavior);
+
   //před vykreslením se vždy provede
   scene.registerBeforeRender(function () {
     //sphere.position.x += 0.03;
-    // light1.setDirectionToTarget(sphere.position);
+    //light1.setDirectionToTarget(sphere.position);
     if (frezaMesh2.position.x > -1) {
       frezaMesh2.rotate(new Vector3(0, 0, 1), (frezaMesh2.rotation.y += 0.01));
     }
 
     //frezaMesh2.rotate(new Vector3(0, 0, 1), (frezaMesh2.rotation.y += 0.001));
   });
-  animationFunction(frezaMesh2);
+  var animationFunction = function (frezaMesh2) {
+    const frameRate = 10;
+    const xSlide = new Animation(
+      "xSlide",
+      "position.x",
+      frameRate,
+      Animation.ANIMATIONTYPE_FLOAT,
+      Animation.ANIMATIONLOOPMODE_CYCLE
+    );
+
+    const keyFrames = [];
+
+    keyFrames.push({
+      frame: 0,
+      value: 1
+    });
+
+    keyFrames.push({
+      frame: frameRate,
+      value: -2
+    });
+
+    keyFrames.push({
+      frame: 2 * frameRate,
+      value: 1
+    });
+
+    xSlide.setKeys(keyFrames);
+    frezaMesh2.animations.push(xSlide);
+    scene.beginAnimation(frezaMesh2, 0, 2 * frameRate, true);
+  };
+  //  animationFunction(frezaMesh2);
 };
-var animationFunction = function (frezaMesh2) {
-  const frameRate = 10;
-  const xSlide = new Animation(
-    "xSlide",
-    "position.x",
-    frameRate,
-    Animation.ANIMATIONTYPE_FLOAT,
-    Animation.ANIMATIONLOOPMODE_CYCLE
-  );
 
-  const keyFrames = [];
-
-  keyFrames.push({
-    frame: 0,
-    value: 1
-  });
-
-  keyFrames.push({
-    frame: frameRate,
-    value: -2
-  });
-
-  keyFrames.push({
-    frame: 2 * frameRate,
-    value: 1
-  });
-
-  xSlide.setKeys(keyFrames);
-  frezaMesh2.animations.push(xSlide);
-  scene.beginAnimation(frezaMesh2, 0, 2 * frameRate, true);
-};
-
-initFunction();
+initSetup().then((scene) => {
+  initModelFunction(scene);
+  initModel2Function(scene);
+});
